d65b25b60cd529d997fe3e64e7423c7324b64b82
Jeff Christoffels <jeff.christoffels@student.kuleuven.be>
2023-04-19 11:29:33 +0200

    Documentation

--- Games/basic_reinforcement_learning/task2.py
+++ Games/basic_reinforcement_learning/task2.py
@@ -10,7 +10,7 @@ def create_biased_rock_paper_scissors_game():
       pyspiel.GameType.Information.ONE_SHOT,
       pyspiel.GameType.Utility.ZERO_SUM,
       pyspiel.GameType.RewardModel.TERMINAL,
-      2,        # max num players
+      2,        # max_num_players
       2,        # min_num_players
       True,     # provides_information_state
       True,     # provides_information_state_tensor
@@ -21,10 +21,10 @@ def create_biased_rock_paper_scissors_game():
   game = pyspiel.MatrixGame(
       game_type,
       {},                                                   # game_parameters
-      ["Rock", "Paper", "Scissors"],                        # row_action_names
-      ["Rock", "Paper", "Scissors"],                        # col_action_names
-      [[0, -0.25, 0.5], [0.25, 0, -0.05], [-0.5, 0.05, 0]], # row player utilities
-      [[0, 0.25, -0.5], [-0.25, 0, 0.05], [0.5, -0.05, 0]]  # col player utilities
+      ["Rock", "Paper", "Scissors"],                        # Row player actions
+      ["Rock", "Paper", "Scissors"],                        # Column player actions
+      [[0, -0.25, 0.5], [0.25, 0, -0.05], [-0.5, 0.05, 0]], # Row player utilities
+      [[0, 0.25, -0.5], [-0.25, 0, 0.05], [0.5, -0.05, 0]]  # Column player utilities
   )
   return game
 
@@ -37,7 +37,7 @@ def create_dispersion_game():
       pyspiel.GameType.Information.ONE_SHOT,
       pyspiel.GameType.Utility.GENERAL_SUM,
       pyspiel.GameType.RewardModel.TERMINAL,
-      2,        # max num players
+      2,        # max_num_players
       2,        # min_num_players
       True,     # provides_information_state
       True,     # provides_information_state_tensor
@@ -48,10 +48,10 @@ def create_dispersion_game():
   game = pyspiel.MatrixGame(
       game_type,
       {},                   # game_parameters
-      ["A", "B"],           # row_action_names
-      ["A", "B"],           # col_action_names
-      [[-1, -1], [1, 1]],   # row player utilities
-      [[1, 1], [-1, -1]]    # col player utilities
+      ["A", "B"],           # Row player actions
+      ["A", "B"],           # Column player actions
+      [[-1, -1], [1, 1]],   # Row player utilities
+      [[1, 1], [-1, -1]]    # Column player utilities
   )
   return game
 
@@ -64,21 +64,21 @@ def create_battle_of_the_sexes_game():
       pyspiel.GameType.Information.ONE_SHOT,
       pyspiel.GameType.Utility.GENERAL_SUM,
       pyspiel.GameType.RewardModel.TERMINAL,
-      2,  # max num players
-      2,  # min_num_players
-      True,  # provides_information_state
-      True,  # provides_information_state_tensor
-      False,  # provides_observation
-      False,  # provides_observation_tensor
-      dict()  # parameter_specification
+      2,        # max_num_players
+      2,        # min_num_players
+      True,     # provides_information_state
+      True,     # provides_information_state_tensor
+      False,    # provides_observation
+      False,    # provides_observation_tensor
+      dict()    # parameter_specification
   )
   game = pyspiel.MatrixGame(
       game_type,
       {},               # game_parameters
-      ["O", "M"],       # row_action_names
-      ["O", "M"],       # col_action_names
-      [[3, 2], [0, 0]], # row player utilities
-      [[0, 0], [2, 3]]  # col player utilities
+      ["O", "M"],       # Row player actions
+      ["O", "M"],       # Column player actions
+      [[3, 2], [0, 0]], # Row player utilities
+      [[0, 0], [2, 3]]  # Column player utilities
   )
   return game
 
@@ -91,20 +91,20 @@ def create_prisoners_dilemma_game():
       pyspiel.GameType.Information.ONE_SHOT,
       pyspiel.GameType.Utility.GENERAL_SUM,
       pyspiel.GameType.RewardModel.TERMINAL,
-      2,  # max num players
-      2,  # min_num_players
-      True,  # provides_information_state
-      True,  # provides_information_state_tensor
-      False,  # provides_observation
-      False,  # provides_observation_tensor
-      dict()  # parameter_specification
+      2,        # max num players
+      2,        # min_num_players
+      True,     # provides_information_state
+      True,     # provides_information_state_tensor
+      False,    # provides_observation
+      False,    # provides_observation_tensor
+      dict()    # parameter_specification
   )
   game = pyspiel.MatrixGame(
       game_type,
       {},                   # game_parameters
-      ["C", "D"],           # row_action_names
-      ["C", "D"],           # col_action_names
-      [[-1 -1], [-4, 0]],   # row player utilities
-      [[0, -4], [-3, -3]]   # col player utilities
+      ["C", "D"],           # Row player actions
+      ["C", "D"],           # Column player actions
+      [[-1 -1], [-4, 0]],   # Row player utilities
+      [[0, -4], [-3, -3]]   # Column player utilities
   )
   return game
