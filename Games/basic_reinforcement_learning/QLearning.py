39ca3356ae801df79149a5fcd4157d022adaf6e1
Jeff Christoffels <jeff.christoffels@student.kuleuven.be>
2023-04-27 15:04:59 +0200

    Started QLearning.py

---
+++ Games/basic_reinforcement_learning/QLearning.py
@@ -0,0 +1,73 @@
+
+
+class QLearning:
+    def __init__(self,
+                 learning_rate,
+                 discount_rate,
+                 initial_exploration_rate):
+        self.learning_rate = learning_rate
+        self.discount_rate = discount_rate
+        self.initial_exploration_rate = initial_exploration_rate
+        self.max_exploration_rate = exploration_rate
+        self.min_exploration_rate = 0.01
+        self.exploration_decay_rate = 0.001
+
+    def set_exploration_decay_rate(self, decay_rate):
+        self.exploration_decay_rate = decay_rate
+
+    @staticmethod
+    def choose_action(Q, state):
+        # Probability of epsilon to choose random action
+        if random.random() < exploration_rate:
+            action = random.choice(list(actions.values()))
+        # Probability of (1 - epsilon) to choose action with highest Q-value
+        else:
+            action = np.argmax(Q[state])
+        return action
+
+    # Update the Q-values using epsilon-greedy Q-learning
+    @staticmethod
+    def update_q(Q, state, action, reward, next_state):
+        Q[state][action] += learning_rate * (reward + discount_rate * max(Q[next_state]) - Q[state][action])
+
+    def learn(self, episodes, actions, payoff):
+        exploration_rate = self.initial_exploration_rate
+        nb_actions = len(actions)
+
+        Q1 = np.zeros(nb_actions, nb_actions)
+        Q2 = np.zeros(nb_actions, nb_actions)
+
+        trajectory1 = []
+        trajectory2 = []
+
+        state1 = random.choice(list(states.values()))
+        state2 = random.choice(list(states.values()))
+        for episode in range(episodes):
+            action1 = choose_action(Q1, state1)
+            action2 = choose_action(Q2, state2)
+
+            P1 = [0 for i in range(nb_actions)]
+            P1[np.argmax(Q1[action1])] = 1 - exploration_rate
+            P1 = [x + exploration_rate / nb_actions for x in P1]
+            trajectory1.append(P1)
+
+            P2 = [0 for i in range(nb_actions)]
+            P2[np.argmax(Q2[action2])] = 1 - exploration_rate
+            P2 = [x + exploration_rate / nb_actions for x in P2]
+            trajectory2.append(P2)
+
+            reward1 = payoff[action1][action2]
+            reward2 = payoff[action2][action1]
+
+            next_state1 = action1
+            next_state2 = action2
+            update_Q(Q1, state1, action1, reward1, next_state1)
+            update_Q(Q2, state2, action2, reward2, next_state2)
+
+            exploration_rate = min_exploration_rate + \
+                               (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate * episode)
+
+        return Q1, Q2, trajectory1, trajectory2
+
+
+
